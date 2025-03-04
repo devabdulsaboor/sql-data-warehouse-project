c1134351ca3a25ae4c37f548a9beea7f2b3edeac
Abdul Saboor <123263294+devabdulsaboor@users.noreply.github.com>
2025-03-05 02:33:59 +0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+USE master;
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DATAWarehouse;
+GO
+
+USE DATAWarehouse;
+GO
+
+--Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
